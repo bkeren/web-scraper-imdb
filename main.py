6d2ba6cab60ab8eae1df8fdc23c72d37bb103c3d
birandkoray <birandkoray@gmail.com>
2019-03-26 06:59:56 +0300

    login procedure improved

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 from bs4 import BeautifulSoup
 import datetime
 import boto3
@@ -7,30 +6,36 @@ from time import strftime, localtime
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import os
+import time
 IMDB_LOGIN_URL = '''https://www.imdb.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.imdb.com%2Fap-signin-handler&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=imdb_us&openid.mode=checkid_setup&siteState=eyJvcGVuaWQuYXNzb2NfaGFuZGxlIjoiaW1kYl91cyIsInJlZGlyZWN0VG8iOiJodHRwczovL3d3dy5pbWRiLmNvbS91c2VyL3VyMjM0NzczNjMvP3JlZl89bGduX25iX3Vzcl9wcm9mXzAifQ&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&tag=imdbtag_reg-20'''
 
 
+
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options,service_log_path="/tmp/geckodriver.log")
+
+
 def login_imdb():
-	pass
+	while driver.page_source.find('<YOUR NAME>') == -1:
+		driver.get(IMDB_LOGIN_URL)
+		driver.find_element_by_id("ap_email").send_keys("<email>")
+		driver.find_element_by_id("ap_password").send_keys("<password>")
+		driver.find_element_by_id("signInSubmit").click()
+		time.sleep(5)
+
+
 	 
 
 
 def deal_scrape():
 	content=""
-	options = Options()
-	options.headless = True
-	driver = webdriver.Firefox(options=options,service_log_path="/tmp/geckodriver.log")
-	driver.get(IMDB_LOGIN_URL)
-	driver.find_element_by_id("ap_email").send_keys("<your email>")
-	driver.find_element_by_id("ap_password").send_keys("<your password>")
-	driver.find_element_by_id("signInSubmit").click()
-	if driver.session_id is not None:
-		driver.get("https://www.imdb.com/chart/top")
-		content= BeautifulSoup(driver.page_source,"html5lib")
-		movie_list = content.select(".lister-list tr")
-		unseen_movies = [movie.select(".titleColumn a")[0].get_text() for movie in movie_list if len(movie.select(".seen-widget.seen")) == 0]
-		return "\n".join(unseen_movies)
-	return ["salman"]	
+	login_imdb()
+	driver.get("https://www.imdb.com/chart/top")
+	content= BeautifulSoup(driver.page_source,"html5lib")
+	movie_list = content.select(".lister-list tr")
+	unseen_movies = [movie.select(".titleColumn a")[0].get_text() for movie in movie_list if len(movie.select(".seen-widget.seen")) == 0]
+	return "\n".join(unseen_movies)
 
 def save_file_to_s3(bucket, file_name, data):
 	s3 = boto3.resource('s3')
@@ -39,8 +44,8 @@ def save_file_to_s3(bucket, file_name, data):
 
 def scrape(event, context):
 	data = deal_scrape()
-	today = strftime("%d.%m.%Y %H:%M:%S", localtime())
-	file_name = f"imdb-{today}"
+	today = strftime("%d-%m-%Y$%H:%M:%S", localtime())
+	file_name = f"imdb-{today}.txt"
 	save_file_to_s3('imdb-scraper' , file_name , data)
 	return "success"
 
